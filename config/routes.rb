d7e38d02277dbfd62c611a94b00a45e4cd1671ba
Gaston de la Fare <gastondelafare@gmail.com>
2024-12-02 17:21:08 -0300

    defined routes for all controllers and initial actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,5 @@
 Rails.application.routes.draw do
-  get 'orders/create'
-  get 'orders/my_orders'
-  get 'tickets/update'
-  get 'tickets/destroy'
-  get 'tickets/create'
-  get 'listings/index'
-  get 'listings/show'
-  get 'listings/new'
-  get 'listings/create'
-  get 'listings/edit'
-  get 'listings/update'
-  get 'listings/destroy'
-  get 'events/index'
-  get 'events/show'
+
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -23,4 +10,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+ 
+  get "/events", to: "events#index"
+  get "/events/:id/listings", to: "listings#index"
+  get "/listings/new", to: "listings#new", as: "new_listing"
+  get "/events/:id/listings/:id", to: "listings#show", as: "listing"
+  patch "/events/:id/listings/:id", to: "listings#update"
+  post "/listings", to: "listings#create"
+  get "/my_listings", to: "listings#my_listings", as: "my_listings"
+  delete "/listings/:id", to: "listings#destroy"
+  get "/orders/new", to: "orders#new", as: "new_order"
+  post "/orders", to: "orders#create"
+  get "/my_orders", to: "orders#my_orders", as: "my_orders"
+  post "/tickets", to: "tickets#create"
+  patch "/tickets/:id", to: "tickets#update"
+  delete "/tickets/:id", to: "tickets#destroy"
+  
 end
